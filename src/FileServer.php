eb3dfcc980c1f4f68128907ec8517ee585f5ee4f
Pader <ypnow@163.com>
2021-05-07 18:44:34 +0800

    Use PSR Resposne for file server.

--- src/FileServer.php
+++ src/FileServer.php
@@ -3,8 +3,8 @@
 namespace Wind\Web;
 
 use Wind\Base\Config;
+use Wind\Web\Response;
 use Workerman\Protocols\Http\Request;
-use Workerman\Protocols\Http\Response;
 
 class FileServer
 {
@@ -22,23 +22,17 @@ class FileServer
         $config = $config->get('server.static_file');
         $path = $config['document_root'].'/'.$filename;
 
-        $response = null;
-
         if ($config['enable_negotiation_cache'] && is_file($path)) {
             if (!empty($ifModifiedSince = $request->header('if-modified-since'))) {
                 $modifiedTime = date('D, d M Y H:i:s',  filemtime($path)) . ' ' . \date_default_timezone_get();
                 // 文件未修改则返回304
                 if ($modifiedTime === $ifModifiedSince) {
-                    $response = new Response(304);
+                    return new Response(304);
                 }
             }
         }
 
-        if ($response === null) {
-            $response = (new Response())->withFile($path);
-        }
-
-        return $response;
+        return (new Response())->withHeader('X-Workerman-Sendfile', $path);
     }
 
 }
