3dd3ab3d92b4fc642e53205334fa05b534df44eb
Pader <ypnow@163.com>
2021-05-07 18:42:53 +0800

    Add AccessLogMiddleware

---
+++ src/Middleware/AccessLogMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Wind\Web\Middleware;
+
+use Wind\Log\LogFactory;
+use Wind\Web\MiddlewareInterface;
+use Wind\Web\RequestInterface;
+
+class AccessLogMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * @var \Monolog\Logger
+     */
+    protected $logger;
+
+    public function __construct(LogFactory $logFactory)
+    {
+        $this->logger = $logFactory->get('access');
+    }
+
+    public function process(RequestInterface $request, callable $handler) {
+        /**
+         * @var \Psr\Http\Message\ResponseInterface $response
+         */
+        $response = yield $handler($request);
+
+        $uri = $request->getUri();
+        $user = $uri->getUserInfo();
+
+        if ($user && ($p = strpos($user, ':')) !== false) {
+            $user = substr($user, 0, $p);
+        }
+
+        $log = sprintf(
+            '%s - %s "%s %s HTTP/%s" %d %d "%s" "%s"',
+            $request->getClientIp(),
+            $user ?: '-',
+            $request->getMethod(),
+            $uri->getPath(),
+            $request->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getBody()->getSize(),
+            $request->getHeaderLine('Referer') ?: '-',
+            $request->getHeaderLine('User-Agent')
+        );
+
+        $this->logger->info($log);
+
+        return $response;
+    }
+
+}
